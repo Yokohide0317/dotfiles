8db8749e260dcdb033fc722613a86212c9c357f9
Yokohide0317 <yokohide0317@gmail.com>
2022-02-14 19:30:15 +0900

    kitty

--- make_symbolic_links.sh
+++ make_symbolic_links.sh
@@ -5,3 +5,5 @@ ln -s "$HOME/dotfiles/neofetch/" "$HOME/.config/neofetch"
 ln -fs "$HOME/dotfiles/vim/m1_mac_vimrc" "$HOME/.vimrc"
 
 ln -fs "$HOME/dotfiles/powerline-shell/" "$HOME/.config/powerline-shell/"
+
+ln -fs "$HOME/dotfiles/kitty/" "$HOME/.config/kitty/"
