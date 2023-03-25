284022a2cb23f6443160090c048b38608f107c2d
Yokohide0317 <yokohide0317@gmail.com>
2023-03-26 08:54:08 +0900

    setup for astronvim

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -9,6 +9,10 @@ cd nerd-fonts
 mkdir ~/.config/nvim
 git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
 
+ln init.lua ~/.config/nvim/
+mkdir ~/.config/nvim/user
+ln user.lua ~/.config/nvim/lua/user/
+
 # nvim
 # :TSInstall python
 # :TSInstall bash
